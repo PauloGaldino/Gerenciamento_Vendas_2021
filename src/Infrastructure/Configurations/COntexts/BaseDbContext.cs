481b72c49a6e9c8b5cc6164819c8ac90052e29d5
José Paulo Galdino <44450145+PauloGaldino@users.noreply.github.com>
2021-10-14 20:40:55 -0300

    Adicionado a classe de contexto na camada de infraestrutura

---
+++ src/Infrastructure/Configurations/COntexts/BaseDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Entity.Persons.Identity.Users;
+using Entity.Persons.Identity.Users.UsersManager;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configurations.Contexts
+{
+    public  class BaseDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public BaseDbContext(DbContextOptions<BaseDbContext> options) : base(options) { }
+
+        //Configuação da tabela de usuário do IDentity
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        //configuração das tabelas de gerenciamento de usuários
+        public DbSet<UserType> UserTypes  { get; set; }
+        public DbSet<UserProfile> userProfiles  { get; set; }
+        public DbSet<AccessTypeUser> AccessTypeUsers  { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-PAULO\\SQLEXPRESS;Initial Catalog=Smart_Cd.db_2021;Integrated Security=False;User ID=sa;Password=toor;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            return strCon;
+        }
+
+    }
+}
